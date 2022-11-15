9456117be067567489b4f1bf7bbe2e36cc6ca321
mohammad.yazdi <mohammad.yazdi@partsoftware.com>
2022-11-15 17:25:14 +0330

    Design list section in HomeFragment

--- app/src/main/java/com/example/cheatsheet/ui/HomeFragment.kt
+++ app/src/main/java/com/example/cheatsheet/ui/HomeFragment.kt
@@ -8,10 +8,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,11 +22,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.example.chaetsheet.R
 import com.example.cheatsheet.ui.theme.CheatSheetTheme
+import com.example.cheatsheet.ui.theme.Gray
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import com.google.android.material.chip.Chip
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -58,19 +66,21 @@ class HomeFragment : Fragment() {
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
-                    Greeting(stringResource(id = R.string.label_myFullName))
+                    Greeter(stringResource(id = R.string.label_myFullName))
                     Spacer(modifier = Modifier.height(20.dp))
                     TopImage(
                         painter = painterResource(id = R.drawable.mountain),
                         title = stringResource(id = R.string.label_mountain)
                     )
+                    Spacer(modifier = Modifier.height(20.dp))
+                    ListSection()
                 }
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String) {
+    fun Greeter(name: String) {
         Text(
             text = buildAnnotatedString {
                 withStyle(
@@ -121,6 +131,264 @@ class HomeFragment : Fragment() {
                     color = Color.White
                 )
             }
+        }
+    }
+
+    @Composable
+    fun ListSection() {
+        var inputValueState by remember {
+            mutableStateOf("")
+        }
+
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+
+            InputField(value = inputValueState) {
+                inputValueState = it
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            Row(
+                Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                RoundedButton(
+                    text = stringResource(id = R.string.label_add),
+                    modifier = Modifier
+                        .height(42.dp)
+                        .weight(1f),
+                ) {
+
+                }
+                Spacer(modifier = Modifier.width(24.dp))
+                RoundedButton(
+                    text = stringResource(id = R.string.label_Reset),
+                    modifier = Modifier
+                        .height(42.dp)
+                        .weight(1f),
+                ) {
+                }
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            TabsAndPager()
+        }
+    }
+
+    @Composable
+    fun InputField(value: String, onValueChange: (String) -> Unit) {
+        TextField(
+            value = value,
+            onValueChange = { onValueChange(it) },
+            textStyle = MaterialTheme.typography.body1,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(46.dp),
+            shape = MaterialTheme.shapes.small,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = MaterialTheme.colors.onSurface,
+                backgroundColor = MaterialTheme.colors.surface,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.label_typeSomeThing),
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.onSurface
+                )
+            }
+        )
+    }
+
+    @Composable
+    fun RoundedButton(modifier: Modifier = Modifier, text: String, onClick: () -> Unit) {
+        Button(
+            modifier = modifier,
+            onClick = onClick,
+            shape = MaterialTheme.shapes.small,
+            colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary)
+        ) {
+
+            Text(
+                text = text,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.onPrimary,
+                style = MaterialTheme.typography.button
+            )
+
+        }
+    }
+
+    @OptIn(ExperimentalPagerApi::class)
+    @Composable
+    fun TabsAndPager() {
+        val pagerState = rememberPagerState(pageCount = 2)
+        val selectedIndex = pagerState.currentPage
+        val coroutineScope = rememberCoroutineScope()
+        val listItems = listOf(
+            "Something1",
+            "Something22",
+            "Something333",
+            "Something44",
+            "Something",
+            "Something5555",
+            "Something5555"
+        )
+
+        val tabTitles = listOf(
+            stringResource(id = R.string.label_list),
+            stringResource(id = R.string.label_chip)
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+
+
+            TabRow(
+                selectedTabIndex = selectedIndex,
+                Modifier
+                    .fillMaxWidth()
+                    .background(
+                        shape = MaterialTheme.shapes.small,
+                        color = MaterialTheme.colors.surface
+                    ), backgroundColor = Color.Transparent,
+                indicator = { },
+                divider = { }
+            ) {
+                tabTitles.forEachIndexed { index, title ->
+                    Tab(
+                        selected = index == selectedIndex,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.scrollToPage(index)
+                            }
+                        },
+                        text = {
+                            Text(
+                                text = title,
+                                style = MaterialTheme.typography.button,
+                                color = if (index == selectedIndex)
+                                    MaterialTheme.colors.onPrimary else
+                                    MaterialTheme.colors.onSurface
+                            )
+                        },
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .background(
+                                shape = MaterialTheme.shapes.small,
+                                color = if (index == selectedIndex)
+                                    MaterialTheme.colors.primary else
+                                    Color.Transparent
+                            )
+                    )
+                }
+
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentAlignment = Alignment.TopCenter
+            ) {
+
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier
+                        .fillMaxWidth(), verticalAlignment = Alignment.Top
+
+                ) { page ->
+                    when (page) {
+                        0 -> {
+                            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                                items(listItems.size) {
+                                    ListItem(listItems[it])
+                                }
+
+                            }
+                        }
+                        1 -> {
+                            FlowRow(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ) {
+                                listItems.forEach {
+                                    ChipItem(it)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ListItem(text: String) {
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.body1,
+                    color = Color.Black
+                )
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_menu_option),
+                    contentDescription = stringResource(id = R.string.label_icMenuOpton),
+                    tint = MaterialTheme.colors.onBackground
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .background(Gray)
+            )
+
+        }
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    fun ChipItem(text: String) {
+
+        Chip(
+            modifier = Modifier
+                .padding(end = 16.dp)
+                .background(Color.Transparent),
+            colors = ChipDefaults.chipColors(Color.Transparent),
+            onClick = { /*TODO*/ },
+            border = BorderStroke(width = 1.dp, color = MaterialTheme.colors.onBackground),
+
+            ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.button,
+                color = MaterialTheme.colors.onBackground
+            )
         }
     }
 }
